d56965e0ad6522ef020caec65d1b8f76adb937ee
Sepehr Lajevardi <lajevardi@gmail.com>
2016-10-03 16:54:27 +0330

    Full unit test coverage for findBy*() methods.

--- tests/unit/ElementQueryTest.php
+++ tests/unit/ElementQueryTest.php
@@ -102,14 +102,16 @@ class ElementQueryTest extends UnitSeleniumTestCase
     /**
      * @test
      *
+     * Tests default mechanisms of findBy*() methods.
+     *
      * @param string $api Name of SeleniumTestCase API method.
      * @param array $args API method array of args.
      * @param string $mechanism WebDriverBy mechanism method to be expected to be called.
-     * @param string $alt Parameter to be passed to specified WebDriverBy mechanism method, falls back to $args.
+     * @param string|null $alt Parameter to be passed to specified WebDriverBy mechanism method, falls back to $args.
      *
-     * @dataProvider elementQueryProvider
+     * @dataProvider elementQueryMethodProvider
      */
-    public function hasApisToFindElementsByDifferentCriterias($api, array $args, $mechanism, $alt = null)
+    public function findsElementsWithDifferentMachanism($api, array $args, $mechanism, $alt = null)
     {
         $this->injectMockedWebDriver(
             $this->webDriverMock
@@ -131,17 +133,54 @@ class ElementQueryTest extends UnitSeleniumTestCase
     }
 
     /**
+     * @test
+     *
+     * Tests fallback mechanism of findByBarOrBaz() methods.
+     *
+     * @param string $api Name of SeleniumTestCase API method.
+     * @param array $args API method array of args.
+     * @param array $mechanism WebDriverBy mechanism method to be expected to be called.
+     * @param array $alt Parameter to be passed to specified WebDriverBy mechanism method, falls back to $args.
+     *
+     * @dataProvider elementQueryMethodWithFallbackProvider
+     */
+    public function findsElementsWithDifferentFallbackMachanism($api, array $args, array $mechanism, array $alt)
+    {
+        $this->injectMockedWebDriver(
+            $this->webDriverMock
+                ->shouldReceive('findElements')
+                ->twice()
+                ->with(WebDriverBy::class)
+                ->andReturn([], $expected = 'foundElement')
+                ->getMock()
+        );
+
+        Mockery::mock('alias:' . WebDriverBy::class)
+               ->shouldReceive($mechanism[0])
+               ->once()
+               ->with($alt[0])
+               ->andReturn(Mockery::self())
+               ->shouldReceive($mechanism[1])
+               ->once()
+               ->with($alt[1])
+               ->andReturn(Mockery::self())
+               ->mock();
+
+        $this->assertSame($expected, $this->$api(...$args));
+    }
+
+    /**
      * Element query data provider.
      *
      * We use this provider to test multiple find*() methods with one test.
      *
      * @return array
      */
-    public static function elementQueryProvider()
+    public static function elementQueryMethodProvider()
     {
         return [
             // Pattern:
-            // [$api, $args, $mechanism[, $alt]]
+            // [$api, $args[], $mechanism[, $alt]]
             ['findByName', ['elementName'], 'name'],
             ['findBySelector', ['ul > li .selector'], 'cssSelector'],
             ['findByClass', ['someClass'], 'cssSelector', '.someClass'],
@@ -151,11 +190,8 @@ class ElementQueryTest extends UnitSeleniumTestCase
             ['findByText', ['someText', '*'], 'xpath', "//*[text()='someText']"],
             ['findByBody', ['someText', '*'], 'xpath', "//*[text()='someText']"],
             ['findByPartialText', ['someText', '*'], 'xpath', "//*[contains(text(), 'someText')]"],
-            // NOTE: Value fallback is not being tested:
             ['findByTextOrValue', ['someText', '*'], 'xpath', "//*[text()='someText']"],
-            // NOTE: Value fallback is not being tested:
             ['findByPartialTextOrValue', ['someText', '*'], 'xpath', "//*[contains(text(), 'someText')]"],
-            // NOTE: ID fallback is not being tested:
             ['findByNameOrId', ['someName'], 'name'],
             ['findByLinkText', ['someText'], 'linkText'],
             ['findByLinkPartialText', ['someText'], 'partialLinkText'],
@@ -164,4 +200,34 @@ class ElementQueryTest extends UnitSeleniumTestCase
         ];
     }
 
+    /**
+     * Element query data provider.
+     *
+     * @return array
+     */
+    public static function elementQueryMethodWithFallbackProvider()
+    {
+        return [
+            // Pattern:
+            // [$api, $args[], $mechanism[], $alt[]]
+            [
+                'findByTextOrValue',
+                ['someValue', '*'],
+                ['xpath', 'cssSelector'],
+                ["//*[text()='someValue']", "*[value='someValue']"]
+            ],
+            [
+                'findByPartialTextOrValue',
+                ['someValue', '*'],
+                ['xpath', 'cssSelector'],
+                ["//*[contains(text(), 'someValue')]", "*[value*='someValue']"]
+            ],
+            [
+                'findByNameOrId',
+                ['someId', '*'],
+                ['name', 'id'],
+                ['someId', 'someId']
+            ],
+        ];
+    }
 }
