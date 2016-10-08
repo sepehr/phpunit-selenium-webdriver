f18a33e210de968d5442c0ecac63b1812e8056f4
Sepehr Lajevardi <lajevardi@gmail.com>
2016-10-08 19:38:24 +0330

    Functional test coverage for element interactions.

--- tests/functional/ElementInteractionTest.php
+++ tests/functional/ElementInteractionTest.php
@@ -10,14 +10,51 @@ class ElementInteractionTest extends FunctionalSeleniumTestCase
 {
 
     /** @test */
-    public function typesIntoAnElement()
+    public function typesIntoAnInputElement()
     {
         $this->visitTestFile()
-            ->type($expected = 'Life is a magnificent illusion', $target = 'findMeByName');
+            ->type($expected = 'Life is a magnificent illusion...', $target = 'typeIntoMe');
 
         $this->assertSame(
             $expected,
-            $this->findByName($target)->getAttribute('value')
+            $this->findById($target)->getAttribute('value')
         );
+    }
+
+    /** @test */
+    public function clicksOnANonLinkElement()
+    {
+        $this->visitTestFile()->click($target = 'onClickChange');
+
+        $this->assertSame(
+            'Oh, you clicker!',
+            $this->findById($target)->getText()
+        );
+    }
+
+    /** @test */
+    public function clicksOnALinkAnchor()
+    {
+        $this->visitTestFile()->click('SeleniumHQ');
+
+        $this->assertSame('http://www.seleniumhq.org', $this->url());
+    }
+
+    /** @test */
+    public function clearsAnInput()
+    {
+        $this->visitTestFile()
+            ->type('Something I want to clear right now!', $target = 'findMeByName')
+            ->clear($target);
+
+        $this->assertSame('', $this->findByName($target)->getAttribute('value'));
+    }
+
+    /** @test */
+    public function hitsKeysOnElements()
+    {
+        $this->visitTestFile()->hit('enter', $target = 'hitEnterOnMe');
+
+        $this->assertSame('', $this->findById($target)->getAttribute('value'));
     }
 }
